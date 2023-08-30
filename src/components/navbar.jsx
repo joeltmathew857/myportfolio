e9ea79275adc0ff8b6664cae52d32713d03363ef
joel thomas mathew <joelmathew857@gmail.com>
2023-08-30 23:36:19 +0530

    myPortfolio

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="bg-gray-800 py-8">
+      <div className="container mx-auto text-white">hi</div>
+    </div>
+  );
+};
+
+export default navbar;
