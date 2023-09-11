19ff871888de5c024ff0ab40e2be0f83d6c13df7
joel thomas mathew <joelmathew857@gmail.com>
2023-09-11 20:03:45 +0530

    base

--- src/components/FontMenter/BaseApparel.jsx
+++ src/components/FontMenter/BaseApparel.jsx
@@ -4,12 +4,11 @@ import HeroImgDes from "../../Assets/Images/Base img/hero-desktop.jpg";
 import HeroImgMob from "../../Assets/Images/Base img/hero-mobile.jpg";
 import Arrowicon from "../../Assets/Images/Base img/icon-arrow.svg";
 import LogoIcon from "../../Assets/Images/Base img/logo.svg";
-import errorIcon from "../../Assets/Images/Base img/icon-error.svg";
 
 const BaseApparel = () => {
   return (
     <div className="justify-center flex flex-col md:flex-col lg:flex-row bg-slate-200 md:bg-slate-200 lg:bg-transparent items-center h-screen w-screen">
-      {/*Description*/}
+      {/* Description */}
       <div className="p-8">
         <div className="flex flex-row ">
           <img
@@ -18,7 +17,7 @@ const BaseApparel = () => {
             className="w-full h-full hidden  md:hidden lg:block"
           />
           <div className="absolute top-20 left-10">
-            <img src={LogoIcon} alt="" srcset="" />
+            <img src={LogoIcon} alt="" />
           </div>
         </div>
         <div>
@@ -35,23 +34,23 @@ const BaseApparel = () => {
             className="rounded-full p-4"
             placeholder="Email Address"
           />
-          <button class="rounded-full bg-yellow-500 p-4">
-            <img src={Arrowicon} alt="" srcset="" />
+          <button className="rounded-full bg-yellow-500 p-4">
+            <img src={Arrowicon} alt="" />
           </button>
         </div>
       </div>
 
-      {/*RightSideImage*/}
+      {/* RightSideImage */}
       <div className="">
         <img
           src={HeroImgDes}
           alt=""
-          className="hidden md:hidden lg:block lg:h-screen "
+          className="hidden md:hidden lg:block lg:h-screen"
         />
         <img
           src={HeroImgMob}
           alt=""
-          className="block md:block lg:hidden md:w-screen"
+          className="block md:block sm:block lg:hidden w-screen  md:w-screen absolute top-30 left-10  md:absolute top-30 left-10 sm:absolute top-40 left-30"
         />
       </div>
     </div>
